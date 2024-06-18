2c7d7f6d425be2988ff4ded51475c3a628e05d51
sarmisthasarkar93 <47422831+sarmisthasarkar93@users.noreply.github.com>
2024-06-18 16:33:25 +0530

    Add files via upload

---
+++ learnDokcker/src/main/java/com/example/learnDokcker/LearnDokckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnDokcker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDokckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDokckerApplication.class, args);
+	}
+
+}
---
+++ learnDokcker/src/test/java/com/example/learnDokcker/LearnDokckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnDokcker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDokckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
